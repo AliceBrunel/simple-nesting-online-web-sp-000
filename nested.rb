c066406474d5f46c26cb1ebc3db8b52ae211028c
Alice Brunel <alc.brunel@gmail.com>
2019-07-11 21:42:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -40,6 +40,7 @@ def alan_kay_is_known_for
      }
      
 programmer_hash[:alan_kay][:known_for]
+
 end
 
 def dennis_ritchies_language
@@ -59,6 +60,7 @@ def dennis_ritchies_language
         }
      }
   programmer_hash[:dennis_ritchie][:languages].join
+  
 end
 
 def adding_matz
@@ -86,6 +88,7 @@ def adding_matz
 
 programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP","C"]}
 programmer_hash
+
 end
 
 def changing_alan
@@ -106,6 +109,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def adding_to_dennis
