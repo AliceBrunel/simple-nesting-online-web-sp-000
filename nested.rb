69c8bb6ee4051e5e62c29c05d4f6af73a2bb9e10
Alice Brunel <alc.brunel@gmail.com>
2019-07-11 22:03:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -127,5 +127,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages] = "Assembly"
+     new_info = "Assembly"
+     programmer_hash[:dennis_ritchie][:languages] = new_info
 end
