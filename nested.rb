28c8ea8696e8f3db0c74b4f2d6bef9fe466acf8b
Alice Brunel <alc.brunel@gmail.com>
2019-07-11 21:41:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,8 +106,6 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     programmer_hash[:alan_kay][:known_for] = alans_new_info
-     programmer_hash[:alan_kay][:known_for]
 end
 
 def adding_to_dennis
@@ -128,5 +126,5 @@ def adding_to_dennis
      }
 
 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
-programmer_hash[:dennis_ritchie][:languages][1]
+programmer_hash[:dennis_ritchie][:languages]
 end
